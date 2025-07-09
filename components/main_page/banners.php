85ac89a365099413facd661a194e790dc81df7bd
goldipl <mgodlewski92@gmail.com>
2025-07-09 20:27:26 +0200

    Replace "Więcej" with "More" in banner buttons for consistency in language

--- components/main_page/banners.php
+++ components/main_page/banners.php
@@ -5,7 +5,7 @@
                 <img src="./assets/img/banners/horizontal_green_banner.jpg" alt="banner" width="660" height="368">
             </a>
             <div class="button01">
-                <a href="#">Więcej</a>
+                <a href="#">More</a>
             </div>
         </div>
         <div class="banners_container__right">
@@ -13,7 +13,7 @@
                 <img src="./assets/img/banners/horizontal_pink_banner.jpg" alt="banner" width="660" height="368">
            </a>
            <div class="button01">
-                <a href="#">Więcej</a>
+                <a href="#">More</a>
             </div>
         </div>
     </div>
