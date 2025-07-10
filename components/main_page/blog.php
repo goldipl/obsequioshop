c1d1276a600ed9b3ea662e40035b8f5ddd1d4716
goldipl <mgodlewski92@gmail.com>
2025-07-10 20:58:40 +0200

    Add blog layout styles with responsive design and hover effects

--- components/main_page/blog.php
+++ components/main_page/blog.php
@@ -2,4 +2,33 @@
     <div class="main-desc">
         <h2 class="title">Latest Post on Our Blog</h2>
     </div>
+    <div class="blog">
+        <a href="" class="blog-slot">
+            <div class="blog-slot__img">
+                <img src="./assets/img/blog/blog1.jpg" alt="blog" width="370" height="168">
+            </div>
+            <div class="blog-slot__text">
+                <h3>Surprise your advertising campaigns with the relief printing technique</h3>
+                <span>Elegant, durable, and original. Embossing is a unique way to promote your brand and grab your customers' attention. If you want to learn how to add value to your merchandising items and how to use this method in your next advertising campaigns, check out our biog.</span>
+            </div>
+        </a>
+        <a href="" class="blog-slot">
+            <div class="blog-slot__img">
+                <img src="./assets/img/blog/blog2.jpg" alt="blog" width="370" height="168">
+            </div>
+            <div class="blog-slot__text">
+                <h3>7 actions to implement to reduce your company's carbon footprint</h3>
+                <span>What if your company could combine performance and environmental commitment? In the face of climate challenges, adopting more sustainable practices has become a necessity_ You can reduce your carbon footprint with small, everyday actions. In this article, you'll discover tips and strategies to address them.</span>
+            </div>
+        </a>
+        <a href="" class="blog-slot">
+            <div class="blog-slot__img">
+                <img src="./assets/img/blog/blog3.jpg" alt="blog" width="370" height="168">
+            </div>
+            <div class="blog-slot__text">
+                <h3>What are the promotional gift trends for 2025?</h3>
+                <span>Do you want to boost your brand image, build customer loyalty, or motivate your employees? Stay up to date with the latest promotional gift trends That are making a strong impact in 2025 and discover the tips our founders. Diederik de Koning and Oriol Badia, have for your success</span>
+            </div>
+        </a>
+    </div>
 </section>
