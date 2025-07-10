e392f8e629fa15ec9c3687fc13bb325fcb0dc54b
goldipl <mgodlewski92@gmail.com>
2025-07-10 20:04:58 +0200

    Add styles for popular categories section and update heading element
    - Changed heading from h1 to h2 for better semantic structure.
    - Introduced new styles for the .popular-categories class, including layout and hover effects.
    - Added styles for .popular-categories-slot, including image and text formatting.
    - Ensured proper spacing and responsiveness for category items.

--- components/main_page/popular_categories.php
+++ components/main_page/popular_categories.php
@@ -1,9 +1,58 @@
 <section id="popular_categories">
     <div class="main-desc">
-        <h1 class="title">
+        <h2 class="title">
             <span>Popular</span>,categories
-        </h1>
+        </h2>
         <p>Custom corporate merchandise, cheap promotional products, customized promotional items</p>
     </div>
-
+    <div class="popular-categories">
+        <a href="" class="popular-categories-slot">
+            <div class="popular-categories-slot__img">
+                <img src="./assets/img/popular_categories/tshirts.jpg" alt="category" width="410" height="267">
+            </div>
+            <div class="popular-categories-slot__text">
+                <h3>Cheap custom t-shirt</h3>
+            </div>
+</a>
+        <a href=""  class="popular-categories-slot">
+            <div class="popular-categories-slot__img">
+                <img src="./assets/img/popular_categories/wallets.jpg" alt="category" width="410" height="267">
+            </div>
+            <div class="popular-categories-slot__text">
+                <h3>Adversting notebooks</h3>
+            </div>
+</a>
+        <a href=""  class="popular-categories-slot">
+            <div class="popular-categories-slot__img">
+                <img src="./assets/img/popular_categories/bottles.jpg" alt="category" width="410" height="267">
+            </div>
+            <div class="popular-categories-slot__text">
+                <h3>Custom USB Flash Drives</h3>
+            </div>
+</a>
+        <a href=""  class="popular-categories-slot">
+            <div class="popular-categories-slot__img">
+                <img src="./assets/img/popular_categories/lanyards.jpg" alt="category" width="410" height="267">
+            </div>
+            <div class="popular-categories-slot__text">
+                <h3>Cheap custom lanyards</h3>
+            </div>
+</a>
+        <a href=""  class="popular-categories-slot">
+            <div class="popular-categories-slot__img">
+                <img src="./assets/img/popular_categories/bags.jpg" alt="category" width="410" height="267">
+            </div>
+            <div class="popular-categories-slot__text">
+                <h3>Reusable personalized bags</h3>
+            </div>
+</a>
+        <a href=""  class="popular-categories-slot">
+            <div class="popular-categories-slot__img">
+                <img src="./assets/img/popular_categories/caps.jpg" alt="category" width="410" height="267">
+            </div>
+            <div class="popular-categories-slot__text">
+                <h3>Cheap custom caps</h3>
+            </div>
+</a>
+    </div>
 </section>
