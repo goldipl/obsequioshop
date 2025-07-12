ccabac804093e99327e9764bc889704a8a784e24
goldipl <mgodlewski92@gmail.com>
2025-07-12 16:02:08 +0200

    Add About Us content with company mission and promotional gifts information

---
+++ about-us.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Obsequio | About Us</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body>
+        <header> 
+            <?php include "./components/nav.php"; ?> 
+        </header>
+        <div id="main-wrapper">
+            <div class="breadcrumbs">
+                <ul>
+                    <li class="breadcrumb-item">
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <span>About Us</span>
+                    </li>
+                </ul>
+            </div>
+            <?php include "./components/about_us/about_us_content.php"; ?>
+            <?php include "./components/main_page/our_team.php"; ?> 
+        </div>
+        <footer> 
+            <?php include "./components/footer.php"; ?> 
+        </footer>
+        <script src="./js/mobile_searchbar.js"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
--- components/article/article_content.php
+++ components/article/article_content.php
@@ -4,12 +4,12 @@
         <img src="./assets/img/article/article_img_01.jpg" alt="img" width="775" height="232">
     </div>
     <div class="article_img_txt_box--text">
-        <p class="title">Lorem ipsum</p>
-        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
     </div>
     <div class="article_txt_img_box--text">
-        <p class="title">Lorem ipsum</p>
-        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
     </div>
     <div class="article_txt_img_box--img">
         <img src="./assets/img/article/article_img_02.jpg" alt="img" width="775" height="232">
@@ -27,8 +27,8 @@
         <img src="./assets/img/article/article_img_small_04.jpg" alt="img" width="380" height="232">
     </div>
     <div class="article_txt_img_img_box--text">
-        <p class="title">Lorem ipsum</p>
-        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
     </div>
     <div class="article_txt_img_img_box__images">
         <div class="article_txt_img_img_box__images--img">
